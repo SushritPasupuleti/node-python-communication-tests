4a522647fd34b4ada677b45d9efc30f1a9d5ca03
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-08-13 10:29:35 +0530

    Update index.js

--- node-server/index.js
+++ node-server/index.js
@@ -5,6 +5,12 @@ app.get('/', (req, res) => {
     res.send('Hello There')
 })
 
+app.get('/pi', async(req, res) => {
+    const data = await fetch('http://localhost:7000/pi/4000')
+    const pi = await data.json();
+    res.send('')
+})
+
 
 app.listen(6000, function () {
     console.log('Server running on port 6000');
