e8c88d1dcd6cad147a72776742fd5506bf3f5221
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-08-13 10:03:41 +0530

    Update main.py

--- py-server/main.py
+++ py-server/main.py
@@ -1,10 +1,15 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pi_calc import main as calc
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    return {"message": "Hello There"}
+
+@app.get("/pi")
+def get_pi():
     return {"message": "Hello There"}
