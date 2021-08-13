1fd8512b68d1b615a6d5a9dbbb17679abe2a6adb
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-08-13 21:28:25 +0530

    Update main.py

--- py-server/main.py
+++ py-server/main.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI
 from pi_calc import main as calc
+import pika
 
 app = FastAPI()
 
@@ -9,6 +10,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello There"}
+
 
 @app.get("/pi/{num}")
 def get_pi(num: int):
