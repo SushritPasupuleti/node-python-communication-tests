dd68648428f7c86e709f140f0908bf1cd2f35cb8
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-08-13 09:34:51 +0530

    Update index.js

--- node-server/index.js
+++ node-server/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello There')
+})
+
+
 app.listen(6000, function () {
-  console.log('Server running on port 6000');
+    console.log('Server running on port 6000');
 })
